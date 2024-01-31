7f03a13c48326546d24a04ad5e13f9a4c2e38466
AyeshaKayani97 <ayeshkay1997@gmail.com>
2024-01-31 19:10:30 +0500

    Handle games without an image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp"
 const getCroppedImageUrl = (url:string)=>{
-    if(!url) return "";
+
+    if(!url) return noImage;
     // get all the characters from the beginning of the url all the way  to the media parametere including the media parameter
     const target = "media/"
     const index = url.indexOf(target) + target.length
