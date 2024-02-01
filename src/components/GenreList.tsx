02e7b2306c536f2f9c8e82cd516ee3d977743507
AyeshaKayani97 <ayeshkay1997@gmail.com>
2024-02-01 11:20:22 +0500

    cleaning up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, List, ListItem,Spinner, Button } from "@chakra-ui/react"
+import { HStack, Image, List, ListItem,Spinner, Button, Heading } from "@chakra-ui/react"
 import useGenres, { Genre } from "../hooks/useGenres"
 
 interface Props{
@@ -12,13 +12,15 @@ const GenreList = ({onSelectGenre,selectedGenre}:Props) => {
     if(isLoading) return <Spinner/>
   return (
     <div>
+      <Heading fontSize="2xl" marginBottom={2}>Genres</Heading>
       <List>
         {
             data.map(gen =>
             <ListItem key={gen.id}>
                 <HStack paddingY={5}>
-                    <Image boxSize="32px" borderRadius={8} src={gen.image_background}/>
-                    <Button fontWeight={gen.id === selectedGenre ?.id ? "bold" : "normal" }      fontSize="lg" variant="link" onClick={()=>onSelectGenre(gen)}> {gen.name}</Button>
+                    <Image boxSize="32px" borderRadius={8} src={gen.image_background} objectFit="cover"/>
+                    <Button fontWeight={gen.id === selectedGenre ?.id ? "bold" : "normal" }      fontSize="lg" variant="link" onClick={()=>onSelectGenre(gen)} 
+                    whiteSpace="normal"   textAlign="left"> {gen.name}</Button>
                     {/* <Button fontSize="lg" variant="link" onClick={()=>console.log(gen)}> {gen.name}</Button> */}
 
                 </HStack>
